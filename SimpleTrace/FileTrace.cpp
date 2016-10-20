f03030112f9853f72e2f033d66445af72d6fbbc2
duangduangh <454716288@qq.com>
2016-10-20 09:15:42 +0800

    trace second

--- SimpleTrace/FileTrace.cpp
+++ SimpleTrace/FileTrace.cpp
@@ -1,22 +1,28 @@
 #include "FileTrace.h"
+#include <sstream>
+#include <exception>
+#include <time.h>
 
 
-
+std::string FileTrace::m_fileName; 
+bool FileTrace::m_bisInit; 
+std::mutex FileTrace::m_Mutex; 
 
 FileTrace::FileTrace()
 {
 	m_fs.open(m_fileName, std::ios_base::app | std::ios_base::binary ); 
 }
 
-FileTrace::FileTrace(const char * sContext)
+FileTrace::FileTrace(int itraceLine, const char * streaceContext)
 {
 	m_fs.open(m_fileName, std::ios_base::app | std::ios_base::binary ); 
-	TraceOut(sContext); 
+	SetTrace(itraceLine, streaceContext); 
+	TraceOut(); 
 }
 
 FileTrace::~FileTrace()
 {
-	delete this; 
+	m_fs.close(); 
 }
 
 void FileTrace::Initialize(const char * sfileName)
@@ -40,13 +46,46 @@ void FileTrace::Initialize(const char * sfileName)
 
 }
 
-void FileTrace::TraceOut(const char * sContext)
+void FileTrace::TraceOut()
 {
 	m_Mutex.lock(); 
 
 	m_fs.seekp(std::ios_base::end); 
-	m_fs << sContext; 
-	m_fs << std::endl; 
+	m_fs << m_trace; 
+	m_fs << std::endl;
+	m_trace.clear(); 
 	m_Mutex.unlock(); 
 }
 
+int main()
+{
+	try{
+	FileTrace::Initialize("c:\\aaa\\a.log"); 
+	FileTrace ss; 
+
+	ss.SetTrace(__LINE__, "i'm main"); 
+	std::cout << ss.m_trace << std::endl; 
+	
+	ss.TraceOut(); 
+	}
+	catch(std::exception e)
+	{
+		std::cout << e.what() << std::endl; 
+	}
+
+	
+}
+
+void FileTrace::SetTrace(int itraceLine, const char * streaceContext)
+{
+	m_trace.clear(); 
+	std::stringstream sStream; 
+	time_t tm = time(NULL); 
+	char *ctm = ctime(&tm); 
+	size_t a =  strlen(ctm); 
+	ctm[a - 1] = '\0'; 
+	sStream << ctm << "    line:" << itraceLine << "    " << streaceContext; 
+	std::cout << m_trace << std::endl; 
+	m_trace.append(sStream.str()); 
+	
+}
--- SimpleTrace/FileTrace.h
+++ SimpleTrace/FileTrace.h
@@ -1,26 +1,31 @@
 #pragma once
+#ifndef FILETRACE_HEAD
 
+#define FILETRACE_HEAD
+#define NTOS(a) #a
 #include <iostream>
 #include <fstream>
 #include <mutex>
 
 class FileTrace
 {
+public:
 	static std::string m_fileName; 
 	static bool m_bisInit; 
 	static std::mutex m_Mutex; 
 	std::fstream m_fs; 
+	std::string m_trace; /*  */
 public:
 	FileTrace(void);
-	FileTrace(const char * sContext); 
+	FileTrace(int itraceLine, const char * streaceContext); 
 	~FileTrace(void);
 public:
 	static void Initialize(const char * sfileName); 
-	void TraceOut(const char * sContext); 
+	void TraceOut(); 
+	void SetTrace(int itraceLine, const char * steaceContext); 
 	void Close(); 
 
 };
 
-std::string FileTrace::m_fileName; 
-bool FileTrace::m_bisInit; 
-std::mutex FileTrace::m_Mutex; 
+
+#endif
